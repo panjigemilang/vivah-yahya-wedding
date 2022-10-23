7fdfc6ecf98043c34bbdaaf62371a54e048b08a9
yahya.sahaja <yahya.sahaja@bukalapak.com>
2022-10-23 17:51:46 +0700

    remove discuss hacky

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -12,8 +12,8 @@
     <div class="mt-5 w-full wedding-font text-5xl text-center text-[#ab7029]">
       Wishes
     </div>
-    <div class="mt-10 w-full relative h-[600px] overflow-hidden">
-      <div class="w-full z-20 absolute bottom-0" style="transform: translateY(-10rem);">
+    <div class="mt-10 w-full relative h-[1000px]">
+      <div class="w-full z-20 absolute top-0">
         <Disqus :lazy="false" class="w-full p-10" shortname="https-vivah-yahya-netlify-app" @ready="handleDisqusReady" />
       </div>
     </div>
