72c0bb18dc8d2b8183be57ad763c66185f65b001
yahya.sahaja <yahya.sahaja@bukalapak.com>
2022-10-23 14:46:48 +0700

    remove button on wedding gift

--- src/components/Send.vue
+++ src/components/Send.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="w-full h-screen flex justify-center items-center">
-    <button v-if="!opened" @click="handleClick" class="active:opacity-20 z-20 cursor-pointer text-2xl px-10 py-5 bg-[#ab7029] rounded-full flex items-center text-white">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-[30px]">
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-      </svg>
-      <div class="ml-5 font-semibold">Open Wedding Gift</div>
-    </button>
-    <div v-else class="p-7 rounded-3xl border border-[#ab7029]">
+    <div class="p-7 rounded-3xl border border-[#ab7029]">
       <div class="text-[#ab7029] text-center wedding-font text-5xl">Wedding Gift</div>
       <div class="text-center mt-20 flex justify-center items-center flex-col">
         <div class="font-bold">BRI</div>
@@ -48,10 +40,6 @@ const opened = ref(false);
 
 const vivahRek = 782301004400534;
 const yahyaRek = 1390016600912;
-
-const handleClick = () => {
-  opened.value = true;
-};
 
 const handleCopy = text => {
   window.copyTextToClipboard(text);
