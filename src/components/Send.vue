bed8e303511260e0090edb4fb2c04c33a0d22d63
yahya.sahaja <yahya.sahaja@bukalapak.com>
2022-10-23 14:00:02 +0700

    fix button background

--- src/components/Send.vue
+++ src/components/Send.vue
@@ -15,7 +15,7 @@
         <div>A.n. Haviva Hajaralia Ray</div>
         <div>{{ vivahRek }}</div>
         <button @click="handleCopy(vivahRek)"
-          class="active:opacity-20 mt-3 z-20 cursor-pointer px-4 py-2 text-xs bg-[#ffffff00] rounded-full flex items-center bg-[#ab7029] text-white">
+          class="active:opacity-20 mt-3 z-20 cursor-pointer px-4 py-2 text-xs rounded-full flex items-center bg-[#ab7029] text-white">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-[20px]">
             <path stroke-linecap="round" stroke-linejoin="round"
@@ -29,7 +29,7 @@
         <div>A.n. Yahya Sahaja</div>
         <div>{{ yahyaRek }}</div>
         <button @click="handleCopy(yahyaRek)"
-          class="active:opacity-20 mt-3 z-20 cursor-pointer px-4 py-2 text-xs bg-[#ffffff00] rounded-full flex items-center bg-[#ab7029] text-white">
+          class="active:opacity-20 mt-3 z-20 cursor-pointer px-4 py-2 text-xs rounded-full flex items-center bg-[#ab7029] text-white">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-[20px]">
             <path stroke-linecap="round" stroke-linejoin="round"
