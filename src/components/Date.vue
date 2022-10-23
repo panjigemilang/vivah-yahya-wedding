02dedf62ee007aeb202acf91401aef55e5d78ca3
yahya.sahaja <yahya.sahaja@bukalapak.com>
2022-10-23 14:22:14 +0700

    add location icon

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -22,13 +22,20 @@
         <div class="mt-16">Sabtu, 5 November 2022</div>
         <div>08:00 s.d Selesai</div>
       </div>
-      <div class="w-full mt-20 text-sm" :style="{
+      <div class="w-full mt-20 text-sm flex justify-center items-center flex-col" :style="{
         opacity: bothOpacity,
         transform: `translateY(${akadTranslateX}px)`
       }">
-        Pekan Sei Langgei, Bandar Masilam, Kab. Simalungun
-        <br />
-        Samping Masjid Al Hidayah
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-[#ab7029]">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+        </svg>
+
+        <div class="mt-2">
+          Pekan Sei Langgei, Bandar Masilam, Kab. Simalungun
+          <br />
+          Samping Masjid Al Hidayah
+        </div>
       </div>
     </section>
   </Parallax>
