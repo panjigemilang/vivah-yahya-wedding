f4e85976b146c270e61a366d3223bbd5b26ec62b
Panji Gemilang <panjigemilang31298@gmail.com>
2023-05-02 17:52:11 +0700

    add google URL

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -30,16 +30,26 @@
       </div>
     </div>
     <div
-      class="relative w-full mt-4 text-sm flex flex-col justify-center items-center"
+      class="relative w-full mt-4 text-sm flex flex-col justify-center items-center text-[#2C3A6E]"
     >
-      <img src="/images/location.png" alt="maps" />
+      <p class="animate-pulse text-xs italic">
+        Ketuk peta di bawah untuk membuka di Google Maps
+      </p>
+      <a
+        class="hover:scale-105 transition-all"
+        href="https://maps.app.goo.gl/F1c3YEqj1nUAPCbh6?g_st=iw"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/images/location.png" alt="maps" />
+      </a>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        class="w-6 h-6 text-[#ab7029] animate-bounce absolute top-28 left-24 z-20"
+        class="w-6 h-6 text-[#ab7029] animate-bounce absolute top-32 left-24 z-20"
       >
         <path
           stroke-linecap="round"
@@ -52,7 +62,7 @@
           d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
         />
       </svg>
-      <div class="mt-2 text-[#2C3A6E]">
+      <div class="mt-2">
         Gang Imam, Jalan Melanthon Siregar, Pematangsiantar
         <br />
         Sebrang Masjid Nurul Huda
